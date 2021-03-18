4f0f0e335dbc4c00743b5dde5c610d723b7d90ce
pk <pipalpurus@gmail.com>
2021-03-18 21:44:03 +0545

    changes get add value for js tag

--- src/generate-js-tag/generate-js-tag.controller.ts
+++ src/generate-js-tag/generate-js-tag.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Logger } from '@nestjs/common';
 import { GenerateJsTagService } from './generate-js-tag.service';
-import { Query, Get } from '@nestjs/common';
+import { Query, Get, Param } from '@nestjs/common';
 import { PaginationQueryDto } from '../commom/dto/pagination-query.dto';
 
 @Controller('generate-js-tag')
@@ -11,5 +11,10 @@ export class GenerateJsTagController {
   @Get()
   findAll(@Query() paginationQuery: PaginationQueryDto) {
     return this.gererateJsTagService.findAll(paginationQuery);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.gererateJsTagService.findOne('' + id);
   }
 }
--- src/generate-js-tag/generate-js-tag.service.ts
+++ src/generate-js-tag/generate-js-tag.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AdUnitEntity } from 'src/ad-unit/ad-unit.entity';
 import { Repository } from 'typeorm';
@@ -18,5 +18,40 @@ export class GenerateJsTagService {
       // skip: offset,
       // take: limit,
     });
+  }
+
+  async findOne(id: string) {
+    const adUnit = await this.adUnitReposatory.findOne(id, {
+      relations: ['adUnitBiddersParams', 'adUnitSize'],
+    });
+    if (!adUnit) {
+      throw new NotFoundException(`Coffe #${id} not found`);
+    }
+    console.log('dd', adUnit);
+    const obj = {
+      id: adUnit.id,
+      websiteUrl: adUnit.websiteUrl,
+      adUnitName: adUnit.adUnitName,
+      bidderName1: adUnit.adUnitBiddersParams[0].adUnit,
+      paramsName1: adUnit.adUnitBiddersParams[0].paramsName,
+      paramsValue1: adUnit.adUnitBiddersParams[0].paramsValue,
+      paramsName2: adUnit.adUnitBiddersParams[1].paramsName,
+      paramsValue2: adUnit.adUnitBiddersParams[1].paramsValue,
+      adUnitNumber: adUnit.adUnitSize[0].adUnitName,
+      height: adUnit.adUnitSize[0].height,
+      width: adUnit.adUnitSize[0].width,
+      both: adUnit.adUnitSize[0].both,
+      height1: adUnit.adUnitSize[1].height,
+      width1: adUnit.adUnitSize[1].width,
+      both1: adUnit.adUnitSize[1].both,
+      height2: adUnit.adUnitSize[2].height,
+      width2: adUnit.adUnitSize[2].width,
+      both2: adUnit.adUnitSize[2].both,
+      height3: adUnit.adUnitSize[3].height,
+      width4: adUnit.adUnitSize[3].width,
+      both3: adUnit.adUnitSize[3].both,
+    };
+    console.log('OB', obj);
+    return obj;
   }
 }
